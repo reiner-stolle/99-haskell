16da2ccb3ae6620fdf4466a3ea88e41bac08b530
reiner-stolle <reinerstolle2@gmail.com>
2022-09-30 01:06:50 +0200

    Solved Exercise 21

--- section-3.hs
+++ section-3.hs
@@ -1,14 +1,15 @@
 -- #########Exercise 21##########
 -- Insert an element at a given position into a list.
-
--- Example:
-
--- * (insert-at 'alfa '(a b c d) 2)
--- (A ALFA B C D)
 -- Example in Haskell:
 
 -- λ> insertAt 'X' "abcd" 2
 -- "aXbcd"
+
+insertAt' :: a -> [a] -> Int -> [a]
+insertAt' x [] n     = [x]
+insertAt' x (y:ys) 1 = x:y:ys
+insertAt' x (y:ys) n 
+                    | n > 0 = [y] ++ insertAt' x ys (n-1)
 
 
 -- ##########Problem 22##########
